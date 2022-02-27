7e9c6c3908549801aa0949395ea2c2f62896b1fc
Suyash Singh <suyashsingh.stem@gmail.com>
2022-02-27 16:37:37 +0530

    Changed database

--- Procfile
+++ Procfile
@@ -1,4 +1,4 @@
 web: gunicorn morefit.wsgi
 release: python manage.py makemigrations --no-input
 release: python manage.py migrate --no-input
-heroku ps:scale web=1
+release: python manage.py collectstatic --noinput
--- morefit/settings.py
+++ morefit/settings.py
@@ -76,11 +76,13 @@ WSGI_APPLICATION = 'morefit.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mydatabase',
+        'default': {
+            'ENGINE':'django.db.backends.postgresql_psycopg2',
+        }
     }
-}
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
